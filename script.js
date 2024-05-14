3dc6c1e49e51467f3e6e51aa9f4720fd4c2279a9
Vitor Vieira Machado <164572708+vitorvieira7@users.noreply.github.com>
2024-05-14 16:03:59 -0300

    commit

--- script.js
+++ script.js
@@ -3,25 +3,23 @@ let navbar = document.querySelector('.navbar');
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-
 window.onscroll = () => {
-    sections.array.forEach(sec => {
+    sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
-        let heigth = sec.offsetHeight;
-        let id = sec.getAtrribute('id')
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-        if (top >= offset && top < offset + heigth) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a [href*=' + id + ' ]').classList.add('active')
-            })
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
         }
-
     });
-}
-menuIcon.oneclick = () => {
+};
+
+menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-
-}
+};
